f2b2c88096b21abacb167c20d174c2c9f2219708
anseonghyeok <itnew0987@gmail.com>
2024-01-08 09:17:25 +0900

    JDBC Select 테스트

--- src/com/KoreaIT/java/JDBCAM/Main.java
+++ src/com/KoreaIT/java/JDBCAM/Main.java
@@ -3,7 +3,9 @@ package com.KoreaIT.java.JDBCAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -89,15 +91,61 @@ public class Main {
 
 			} else if (cmd.equals("article list")) {
 				System.out.println("==목록==");
-				if (articles.size() == 0) {
-					System.out.println("게시글이 없습니다");
-					continue;
+
+				Connection conn = null;
+				Statement pstmt = null;
+				ResultSet rs = null;
+				String SQL = "SELECT * FROM article";
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBC_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					pstmt = conn.createStatement();
+
+					rs = pstmt.executeQuery(SQL);
+
+					if (rs==null ) {
+						System.out.println("게시글이 없습니다");
+						continue;
+					}
+
+					System.out.println("==목록==");
+					
+					while (rs.next()) {
+						
+						
+						int id = rs.getInt("id");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+						System.out.println("  번호  /   제목  /  내용  ");
+						System.out.printf("  %d  /   %s  /  %s  ",id,title,body);
+					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 				}
 
-				System.out.println("  번호  /   제목  ");
-				for (Article article : articles) {
-					System.out.printf("  %d     /   %s   \n", article.getId(), article.getTitle());
-				}
 			}
 
 		}
